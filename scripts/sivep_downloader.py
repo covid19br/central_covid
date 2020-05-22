90a4285478c2abfc6f8b39ad7accc05febe91656
Renato Coutinho <renato.coutinho@gmail.com>
2020-05-22 13:37:49 -0300

    downloader sivep não vai mais perder nada

--- scripts/sivep_downloader.py
+++ scripts/sivep_downloader.py
@@ -1,41 +1,65 @@
-import os
-from datetime import date, timedelta
+import os, shutil
+from datetime import date, timedelta, datetime
 from lxml import html 
 import requests 
 import re
- 
-def check_for_new_file(index_page_address, data):
-    page = requests.get(index_page_address, verify=False, timeout=1)
+from glob import glob
+
+def find_last_date(output_folder):
+    files = glob(output_folder + '/*.csv')
+    date_max = date(year=2020, month=1, day=1)
+    for f in files:
+        g = re.match(r'.*(\d\d\d\d_\d\d_\d\d).*.csv', os.path.basename(f))
+        if g:
+            data = datetime.strptime(g.groups()[0], "%Y_%m_%d").date()
+            date_max = max(data, date_max)
+    return date_max
+
+def check_for_new_file(index_page_address, last_date):
+    page = requests.get(index_page_address, verify=False, timeout=10)
     tree = html.fromstring(page.content)
-    l = tree.xpath('//a[starts-with(text(), "Banco de dados de 2020")]')[0]
-    data_read = re.match(r'.*(\d\d/\d\d/\d\d\d\d)', l.text_content()).groups()[0]
-    if data_read == data.strftime("%d/%m/%Y"):
-        return index_page_address + l.attrib['href']
+    resources = tree.xpath('//li[@class="resource-item"]')
+    reg = re.compile(r".*SRAG (\d\d/\d\d/\d\d\d\d).*",
+            re.DOTALL|re.MULTILINE|re.IGNORECASE)
+    for item in resources:
+        g = reg.match(item.text_content())
+        if g:
+            data_read = datetime.strptime(g.groups()[0], "%d/%m/%Y").date()
+            if data_read > last_date:
+                address = item.xpath('.//a[@class="resource-url-analytics"]')[0].attrib['href']
+                return (data_read, address)
     return False
 
 def get_file(download_address, output_file):
-    r = requests.get(download_address, verify=False, allow_redirects=True, timeout=1)
+    r = requests.get(download_address, verify=False, allow_redirects=True, timeout=100)
     open(output_file, 'wb').write(r.content)
+    if download_address[-4:] == 'xlsx':
+        print("Converting xlsx to csv...")
+        xlsfile = output_file[:-3] + 'xlsx'
+        shutil.move(output_file, xlsfile)
+        os.system('libreoffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":59,34,0,1,1 {xlsfile} --outdir {output_dir}'.format(xlsfile=xlsfile, output_dir=os.path.dirname(output_file)))
 
 if __name__ == '__main__':
     data = date.today()
 
-    index_page_address = "http://plataforma.saude.gov.br/coronavirus/dados-abertos/"
+    index_page_address = "https://shiny.hmg.saude.gov.br/tr/dataset/bd-srag-2020"
     output_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../dados/SIVEP-Gripe')
-    output_fname = "SRAGHospitalizado_{data}.csv".format(data=data.strftime("%Y_%m_%d"))
-    outfile = os.path.join(output_folder, output_fname)
-    gitUpdate = False
+    gitUpdate = True
 
-    newfile = check_for_new_file(index_page_address, data)
-    if not os.path.exists(outfile) and newfile:
+    last_date = find_last_date(output_folder)
+    newfile = check_for_new_file(index_page_address, last_date)
+    if newfile:
+        output_fname = "SRAGHospitalizado_{data}.csv".format(data=newfile[0].strftime("%Y_%m_%d"))
+        outfile = os.path.join(output_folder, output_fname)
         print("Downloading new SIVEP database...")
-        get_file(newfile, outfile)
+        get_file(newfile[1], outfile)
         # add to git and let the other robots work
         if gitUpdate:
             os.system('''cd {folder} &&
                    git add {outfile} &&
-                   git commit -m "[auto] base esus-ve de hoje" &&
+                   git commit -m "[auto] base SIVEP-Gripe de {data}" &&
                    git push'''.format(folder=output_folder,
-                                    outfile=output_fname))
+                                    outfile=output_fname,
+                                    data=newfile[0].strftime("%Y_%m_%d")))
 
 
