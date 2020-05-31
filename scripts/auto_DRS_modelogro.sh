a2daa332ddcb0fdfaac4e9455a1b20f08e85e9e1
Renato Coutinho <renato.coutinho@gmail.com>
2020-05-31 18:46:14 -0300

    pequenas correção auto DRS modelogro

--- scripts/auto_DRS_modelogro.sh
+++ scripts/auto_DRS_modelogro.sh
@@ -53,11 +53,11 @@ if [[ $newcommit && -f $csv2 && ! -f $out && ! -f $RUNFILE ]]; then
             --check_report TRUE &&
         cd $absdatafolder
         git pull &&
-        git add outputs/projecao_leitos/DRS/$estado/${nomes_DRS[$nDRS]}/curve_fits/curve_fits_${todaydash}.Rds &&
-        git add outputs/projecao_leitos/DRS/$estado/$nomeDRS/hospitalizados/hopitalized_${todaydash}.csv &&
+        git add outputs/projecao_leitos/DRS/$estado/${nomes_DRS[$DRS]}/curve_fits/curve_fits_${todaydash}.Rds &&
+        git add outputs/projecao_leitos/DRS/$estado/${nomes_DRS[$DRS]}/hospitalizados/hopitalized_${todaydash}.csv &&
         git add dados/municipio_SP/projecao_leitos/hospitalizados/hopitalized_UTI_${todaydash}.csv &&
         git add relatorios/${todaydash}_relatorio_projecoes_demanda_hospitalar_srag.pdf &&
-        git commit -m ":robot: projecao leitos DRS ${estado}-${nomes_DRS[$nDRS]}" &&
+        git commit -m ":robot: projecao leitos DRS ${estado}-${nomes_DRS[$DRS]}" &&
         # DANGER: rebase é perigo: mantenha sua cópia local em ordem!
         # por outro lado, é a única solução com robôs concorrentes em outra máquina.
         git pull --rebase &&
