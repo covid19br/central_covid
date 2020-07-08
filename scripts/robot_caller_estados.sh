b4d2141ea37c832dbf1b7ea24eefb2dc737e92c3
Renato Coutinho <renato.coutinho@gmail.com>
2020-07-08 04:13:33 -0300

    rodando estados numa ordem mais logica

--- scripts/robot_caller_estados.sh
+++ scripts/robot_caller_estados.sh
@@ -11,7 +11,9 @@ touch $RUNFILE
 
 source functions.sh
 
-for geocode in ${!ESTADOS[@]}; do
+IFS=$'\n' estados_ord=($(sort <<<"${!ESTADOS[*]}"))
+unset IFS
+for geocode in ${estados_ord[@]}; do
     echo "Rodando ./auto_site_escala.sh estado ${ESTADOS[$geocode]} $trim $geocode"
     ./auto_site_escala.sh estado ${ESTADOS[$geocode]} $trim $geocode
 done
