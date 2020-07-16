e5cb1a7af94d4ebd5f3854556dbc5b47ef954a65
Andrea Sánchez Tapia <katori@gmail.com>
2020-07-15 20:06:39 -0700

    format table esus + nowcasting de teste

--- scripts_R_genericos/summary_esus/atrasos_bases_esusve.R
+++ scripts_R_genericos/summary_esus/atrasos_bases_esusve.R
@@ -1,50 +1,111 @@
-
 library(tibble)
 library(reshape)
 library(tidyr)
-install.packages("splitstackshape")
 library(splitstackshape)
+library(dplyr)
+library(foreign)
+library(stringr)
+library(lubridate)
+library(R.utils)
+library(readr)
 
-munFileName <- "summary_esus_Florianopolis_v3.csv"
-tudo<-read.csv(munFileName, sep=";")
-names(tudo)<-c("datainiciosintomas", "n.casos", "data.base")
+mun.name <- "FlorianÃ³polis"
+munFileName <-  paste0("./scripts_R_genericos/summary_esus/Florianopolis/summary_esus_", mun.name,".csv")
 
-###código leitura Andréia######
-
-tudo <- read.csv(munFileName) %>%
-  arrange(data.base, datainiciosintomas) %>%
+tudo <- read.csv(
+  munFileName,
+  col.names = c("datainiciosintomas", "n.casos", "data.base")) %>%
+  arrange(datainiciosintomas, data.base) %>%
   filter(!is.na(datainiciosintomas))
 
-data_wide <- tidyr::pivot_wider(tudo, names_from = data.base, values_from = n.casos) %>% data.frame()
-
+#filtra a primeira apariÃ§Ã£o do caso
+data_wide <- tudo %>%
+  distinct(datainiciosintomas, n.casos, .keep_all = TRUE)
+data_wide <-
+  tidyr::pivot_wider(no_dupls,
+                     names_from = data.base,
+                     values_from = n.casos) %>%
+  data.frame()
+data_wide[is.na(data_wide)] <- 0
+data_wide
 names(data_wide) <- str_remove(string = names(data_wide),pattern = "X")
 
-row.names(data_wide)<-data_wide$datainiciosintomas
-data_wide<-data_wide[,-1]
-data_wide[is.na(data_wide)]<-0
+data_wide <- tibble::column_to_rownames(.data = data_wide,
+                                        var = "datainiciosintomas")
 
-##########calculando a diferença entre as bases - Tati - adaptação do método Rafa #######
+diff_se_maior <- function(x, y) {
+  z <- if_else(y >= x,  y - x, 0)
+  return(z)
+  }
 
-##aqui calcula a diferença entre os casos acumulados por data de sintoma de uma base para outra. 
-#Isso gera um novo data.frame com número de casos novos por data de sintoma e data de base
-
-nc <- ncol(data_wide)
-d <- data_wide[-1] - data_wide[-nc]
-
-####aqui gera um novo data frame com data de sintoma e data de notificação para cada caso
-
-d<-tibble:: rownames_to_column(d,"dataInicioSintomas")
-table2<- reshape::melt (d, "dataInicioSintomas")
-table3<-expandRows(table2,"value")   ####não está dando certo porque tem valor negativo 
-
-####tem que ver como conferir e o que fazer com os valores negativos####
+for (i in 1:(ncol(data_wide - 1))) {
+  data_diff[,i] <- diff_se_maior(data_wide[,i], data_wide[,i + 1])
+  }
 
 
+dados_mun <- data_diff %>%
+  rownames_to_column(var = "data_inicio") %>%
+  pivot_longer(cols = -1, names_to = "data_base") %>%
+  filter(value != 0) %>%
+  mutate(data_inicio = lubridate::as_date(data_inicio),
+         data_base = lubridate::as_date(data_base)) %>%
+  data.frame()
+
+window <- 40
+dados.now <- NobBS::NobBS(data = dados_mun,
+                          now = max(data_now$data_base), #- trim.now,
+                          onset_date = "data_inicio",
+                          report_date = "data_base",
+                          units = "1 day",
+                          moving_window = window)
+
+now.dados.mun <- dados.now$estimates
+now.dados.mun$estimate <- as.integer(now.dados.mun$estimate)
+now.dados.mun$lower <- as.integer(now.dados.mun$lower)
+now.dados.mun$upper <- as.integer(now.dados.mun$upper)
 
 
+###agregando casos por data de sintoma############
+
+casos_mun <- dados_mun %>%
+  group_by(data_inicio) %>%
+  summarise(Casos = n())  %>%
+  as.data.frame()
 
 
+#####juntando os casos estimados com reais#####
+
+names(casos_mun) <- c("onset_date","estimate")
+casos_mun_f <- casos_mun %>%
+  filter(data_inicio < min(now.dados.mun$onset_date))
+
+casos_mun_f$lower <- casos_mun_f$estimate
+casos_mun_f$upper <- casos_mun_f$estimate
+
+now.dados.mun <- now.dados.mun %>%
+  select(estimate,onset_date, lower, upper)
+
+casos.now.mun <- bind_rows(casos_mun_f, now.dados.mun)
 
 
+###########fazendo a cumulativa####################
+cum.casos.now.mun <- casos.now.mun
+cum.casos.now.mun$estimate <- cumsum(casos.now.mun$estimate)
+cum.casos.now.mun$lower <- cumsum(casos.now.mun$lower)
+cum.casos.now.mun$upper <- cumsum(casos.now.mun$upper)
 
 
+######cortando data para o grÃ¡fico###########
+ggplot(casos.now.mun, aes(x = onset_date, y = estimate)) +
+  geom_line(data = casos_mun, aes(x = onset_date, y = estimate), size = 1) +
+  geom_line(data = casos.now.mun,
+            aes(x = onset_date, y = estimate), size = 1, col = "red") +
+  geom_ribbon(data = casos.now.mun, aes(ymin = lower, ymax = upper),
+              fill = RColorBrewer::brewer.pal(3, "Set1")[2], alpha = 0.5) +
+  scale_color_manual(name = "", values = RColorBrewer::brewer.pal(3, "Set1")[1:2]) +
+  xlab("Data primeiro Sintoma") +
+  ylab("NÃºmero novos casos leves/dia") +
+  theme_minimal() +
+  ggtitle("Casos leves de COVID em FlorianÃ³polis") +
+  theme(legend.position = c(0.2, 0.8))
+ggsave("./scripts_R_genericos/summary_esus/Florianopolis.png")
