e6f5d1f84ebef5c396343f517033a89b24dc1656
piklprado <paulo@PI.localdomain>
2020-08-07 01:38:52 -0300

    O

---
+++ scripts_R_genericos/obitos_seade.R
@@ -0,0 +1,4 @@
+seade <- read.csv2("https://raw.githubusercontent.com/seade-R/dados-covid-sp/master/data/casos_obitos_doencas_preexistentes.csv")
+summary(seade)
+
+betas.sp <- read.csv("https://raw.githubusercontent.com/covid19br/R_eff/master/graficos_paper/covid.beta2020_05_31.csv?token=ABQFLFIQJEU644GRNMBFZBS7FCDBK")
