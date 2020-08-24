4fbf93d8396d9784378c6b4ac08108dac2fe806e
piklprado <paulo@PI.localdomain>
2020-08-24 02:16:19 -0300

    Comparacao nowcastings Sampa

--- scripts_R_genericos/compara_nowcastings.R
+++ scripts_R_genericos/compara_nowcastings.R
@@ -8,14 +8,19 @@ library(stringr)
 library(RCurl)
 library(car)
 library(bbmle)
-source("../nowcasting/fct/get.last.date.R")
-source("../nowcasting/fct/get_dates.R")
+library(purrr)
+library(devtools)
+## source("../nowcasting/fct/get.last.date.R")
+## source("../nowcasting/fct/get_dates.R")
+## source("../nowcasting/fct/get.data.base.R")
+## source("../nowcasting/fct/check.geocode.R")
+load_all("../../now_fcts/")
 
 ################################################################################
 ## leitura das últimas  planilhas de nowcasting de casos do Observatório
 ################################################################################
 data.dir <- "../site/dados/municipios/SP/Sao_Paulo/tabelas_nowcasting_para_grafico/"
-datas <- get_dates(data.dir)
+datas <- get.data.base(data.dir, last=FALSE)
 f1 <- function(data, data.dir){
     data.lab <- format(data, "%Y_%m_%d")
     dados <-  read.csv(paste0(data.dir,"nowcasting_diario_srag_", data.lab,".csv"))
@@ -33,3 +38,23 @@ ldply( lapply(rev(datas)[1:n], f1, data.dir = data.dir)) %>%
     geom_ribbon(aes(ymin=lower.merged.pred, ymax = upper.merged.pred, fill = factor(dt_base)), alpha =0.1) +
     theme_bw()
 
+################################################################################
+## Comparacao de nowcasting de 40 dias atrás com o observado 40 dias após
+################################################################################
+source("comparacao_bases/format_all_nowcasting.R")
+source("comparacao_bases/check_diario.R")
+
+diario <- format_nowcasting_output(
+  dir = "../site/dados/",
+  escala = "municipio",
+  geocode = "3550308",
+  analise = c("nowcasting_diario"))
+
+datas_now <- get.data.base(names = list.files(data.dir), last = FALSE)
+## Que data está a 40 dias da última
+data_fim <- last(datas_now[as.Date(datas_now,"%Y_%m_%d") <= as.Date(datas_now[length(datas_now)],"%Y_%m_%d")-40])
+compara_nowcastings_diarios(df = diario, 
+                            tipo = "srag",
+                            data_inicial = data_fim,
+                            data_final = datas_now[length(datas_now)],
+                            plot.points = FALSE) + ggtitle("COVID")
--- scripts_R_genericos/comparacao_bases/check_diario.R
+++ scripts_R_genericos/comparacao_bases/check_diario.R
@@ -1,43 +1,51 @@
 compara_nowcastings_diarios <- function(df = all2,
-                                      tipo_caso = "covid",
-                                      data_inicial = c("2020_03_26"),
-                                      data_final = c("2020_07_07"),
-                                      dates_after = 7) {
-  if (missing(data_final) & !missing(dates_after)) {
-    datas <- unique(df$data_analise)
-    data_final <- datas[which(datas == data_inicial) + dates_after]
-  }
-  datas <- c(data_inicial, data_final)
-
-  dff <-  df %>%
-    dplyr::filter(tipo == tipo_caso) %>%
-    dplyr::filter(data_analise %in% datas) %>%
-    mutate(data = as.Date(data))
-  antes <- dff %>% filter(data_analise == datas[1])
-  depois <- dff %>% filter(data_analise == datas[2])
-  end <- depois$data[which(is.na(depois$n.casos))[1]]
+                                        tipo_caso = "covid",
+                                        data_inicial = c("2020_03_26"),
+                                        data_final = c("2020_07_07"),
+                                        dates_after = 7,
+                                        plot.points = TRUE) {
+    if (missing(data_final) & !missing(dates_after)) {
+        datas <- unique(df$data_analise)
+        data_final <- datas[which(datas == data_inicial) + dates_after]
+    }
+    datas <- c(data_inicial, data_final)
+    
+    dff <-  df %>%
+        dplyr::filter(tipo == tipo_caso) %>%
+        dplyr::filter(data_analise %in% datas) %>%
+        mutate(data = as.Date(data))
+    antes <- dff %>% filter(data_analise == datas[1])
+    depois <- dff %>% filter(data_analise == datas[2])
+    end <- depois$data[which(is.na(depois$n.casos))[1]]
     depois <- depois %>% filter(data < end)
-
+    
   plot <- antes %>%
-    mutate(data = as.Date(data)) %>%
-    ggplot(aes(x = data)) +
-    #geom_point(aes(y = n.casos, col = paste("Notificados", data[1])), size = 2) +
-    geom_ribbon(aes(ymin = lower.merged.pred, ymax = upper.merged.pred),
-                fill = RColorBrewer::brewer.pal(3, "Set2")[1], alpha = 0.5) +
-    geom_line(aes(y = estimate.merged.smooth), alpha = 0.6, size = 1.5, col = RColorBrewer::brewer.pal(3, "Set2")[1]) +
-    geom_line(data = depois, aes(y = estimate.merged.smooth, x = data), alpha = 0.6, col = "red", size = 1.2) +
-    geom_point(data = depois, aes(y = n.casos, col = paste("Número de casos", datas[2])), size = 2) +
-    geom_point(data = antes, aes(y = n.casos, col = paste("Nowcasting", datas[1])), size = 2) +
-    geom_ribbon(data = depois, aes(ymin = lower.merged.pred, ymax = upper.merged.pred, fill = RColorBrewer::brewer.pal(3, "Set2")[2]),
-                fill = RColorBrewer::brewer.pal(3, "Set2")[2], alpha = 0.5) +
-    scale_x_date(date_labels = "%d/%b") +
-    scale_color_manual(name = "", values = RColorBrewer::brewer.pal(3, "Set2")[1:3]) +
-    xlab("Dia do primeiro sintoma") +
-    ylab("Número de novos casos") +
-    theme_classic() +
-    theme(legend.position = c(0.2, 0.8),
+      mutate(data = as.Date(data)) %>%
+      ggplot(aes(x = data)) +
+      ##geom_point(aes(y = n.casos, col = paste("Notificados", data[1])), size = 2) +
+      geom_ribbon(aes(ymin = lower.merged.pred, ymax = upper.merged.pred),
+                  fill = RColorBrewer::brewer.pal(3, "Set2")[1], alpha = 0.5) +
+      geom_line(aes(y = estimate.merged.smooth, col = paste("Nowcasting", datas[1])), alpha = 0.6, size = 1.5) +
+      geom_line(data = depois, aes(y = estimate.merged.smooth, x = data, col = paste("Número de casos", datas[2])),
+                alpha = 0.6, size = 1.2) +
+      ##geom_point(data = depois, aes(y = n.casos, col = paste("Número de casos", datas[2])), size = 2) +
+      ##geom_point(data = antes, aes(y = n.casos, col = paste("Nowcasting", datas[1])), size = 2) +
+      geom_ribbon(data = depois, aes(ymin = lower.merged.pred, ymax = upper.merged.pred),
+                  fill = RColorBrewer::brewer.pal(3, "Set2")[2], alpha = 0.5) +
+      scale_x_date(date_labels = "%d/%b") +
+      scale_color_manual(name = "", values = RColorBrewer::brewer.pal(3, "Set2")[1:3]) +
+      xlab("Dia do primeiro sintoma") +
+      ylab("Número de novos casos") +
+      theme_classic() +
+      theme(legend.position = c(0.2, 0.8),
           legend.title = element_blank()) +
-    geom_vline(xintercept = antes$data[which(is.na(antes$n.casos))[1] - 1]) +
-    geom_vline(xintercept = max(antes$data))
-  plot
+      geom_vline(xintercept = antes$data[which(is.na(antes$n.casos))[1] - 1]) +
+      geom_vline(xintercept = max(antes$data))
+    if(plot.points) {
+        plot +
+            geom_point(data = depois, aes(y = n.casos), col = "red", size = 2) +
+            geom_point(data = antes, aes(y = n.casos), col = RColorBrewer::brewer.pal(3, "Set2")[1], size = 2)
+        }
+    else
+        plot
 }
