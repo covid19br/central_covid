c4f4c01537418ccf6c4e45a43a481d1376050610
piklprado <paulo@PI.localdomain>
2021-01-22 03:20:34 -0300

    relatorio InforGripe e plots casos SEADE

--- scripts_R_genericos/plano_SP/casos_seade/casos_por_drs.R
+++ scripts_R_genericos/plano_SP/casos_seade/casos_por_drs.R
@@ -3,13 +3,21 @@ library(dplyr)
 library(ggplot2)
 library(zip)
 library(aweek)
+set_week_start("sunday")
+
+## Em 21/01/2021 commits posteriores a 15/01 parece ter tabelas com linhas faltantes
+## Usando o ultimo commit que parece bem
+system("cd ../../../clone_repo_seade_SP/; git checkout 89c35a9")
 
 ################################################################################
 ## SEADE: Tabela com codigos municipios, DRS e populacao
 ################################################################################
 ## Leitura do arquivo com os dados de notificacao, que tem os dados dos municipios
-not.mun <- read.csv2("https://raw.githubusercontent.com/seade-R/dados-covid-sp/master/data/dados_covid_sp.csv")
-## Seleciona as varaveis para usar na tabela de muncipios e retém apenas um resgistor por município
+not.mun <- read.csv2("../../../clone_repo_seade_SP/data/dados_covid_sp.csv") %>%
+    mutate(datahora = as.Date(datahora)) %>%
+    filter(datahora < as.Date("2021-01-03"))
+
+## Seleciona as varaveis para usar na tabela de muncipios e retém apenas um resgistro por município
 dic.mun <-
     not.mun %>%
     select(nome_munic,codigo_ibge,nome_ra,cod_ra,nome_drs,cod_drs,pop,pop_60,area,latitude,longitude) %>%
@@ -31,7 +39,7 @@ not.drs <-
     summarise(casos_not = sum(casos_novos),
               obitos_not = sum(obitos_novos)) %>%
     as.data.frame() %>%
-    merge(drs) %>%
+    merge(dic.drs) %>%
     mutate(casos_not_pc = 1e5*casos_not/pop, obitos_not_pc = 1e5*obitos_not/pop) %>%
     select(nome_drs:cod_drs,casos_not_pc, obitos_not_pc)
 
@@ -39,12 +47,18 @@ not.drs <-
 ## SEADE: Casos por data de sintoma
 ################################################################################
 ## Leitura dos dados
-download.file("https://github.com/seade-R/dados-covid-sp/raw/master/data/casos_obitos_doencas_preexistentes.csv.zip",
-              destfile = "casos_obitos_doencas_preexistentes.csv.zip")
-zip::unzip("casos_obitos_doencas_preexistentes.csv.zip", junkpaths = TRUE)
+##download.file("https://github.com/seade-R/dados-covid-sp/raw/master/data/casos_obitos_doencas_preexistentes.csv.zip",
+##              destfile = "casos_obitos_doencas_preexistentes.csv.zip")
+
+## é preciso um clone do respositorio do SEAD neste path
+zip::unzip("../../../clone_repo_seade_SP/data/casos_obitos_doencas_preexistentes.csv.zip", junkpaths = TRUE)
+## zip::unzip("casos_obitos_doencas_preexistentes.csv_2021_01_15.zip", junkpaths = TRUE)
+
 casos.all <- read.csv2("casos_obitos_doencas_preexistentes.csv") %>%
-    mutate(data_inicio_sintomas = as.Date(data_inicio_sintomas, "%m/%d/%Y"),
-           semana_epidem = date2week(data_inicio_sintomas, numeric = TRUE))
+    mutate(data_inicio_sintomas = as.Date(data_inicio_sintomas)) %>%  
+    filter(data_inicio_sintomas < as.Date("2021-01-03")) %>% ## para filtrar apenas semanas epidemiológicas de 2020
+    mutate(semana_epidem = date2week(data_inicio_sintomas, numeric = TRUE)) 
+    
 
 casos.semana <-
     casos.all %>%
@@ -52,7 +66,7 @@ casos.semana <-
     group_by(nome_munic, semana_epidem) %>%
     summarise(casos_novos = n(), obitos_novos = sum(obito ==1)) %>%
     ungroup() %>%
-    inner_join(dic.mun[,c("nome_munic", "nome_drs", "cod_drs")], by = "nome_munic")
+    merge(dic.mun[,c("nome_munic", "nome_drs", "cod_drs")], by = "nome_munic", all.x=TRUE)
     
 casos.semana.drs <-
     casos.semana %>%
@@ -60,7 +74,7 @@ casos.semana.drs <-
     summarise(casos_sin = sum(casos_novos),
               obitos_sin = sum(obitos_novos)) %>%
     ungroup() %>%
-    inner_join(drs, by = "nome_drs") %>% 
+    inner_join(dic.drs, by = "nome_drs") %>% 
     mutate(casos_sin_pc = 1e5*casos_sin/pop, obitos_sin_pc = 1e5*obitos_sin/pop) %>%
     inner_join(not.drs, by = c("nome_drs", "semana_epidem")) %>%
     select(nome_drs, semana_epidem, cod_drs.x, pop, pop_60, area, casos_not, casos_sin, casos_not_pc, casos_sin_pc,
@@ -115,7 +129,7 @@ write.csv(maximos.sin , file = "outputs/totais_maximos_e_semana_pico_semana_sint
 png("outputs/casos_por_semana.png", width =900, height = 600)
 casos.semana.drs %>%
     ##filter(semana_epidem < max(semana_epidem)-1) %>%
-    filter(semana_epidem < 42) %>%
+    ##filter(semana_epidem < 42) %>%
     ggplot(aes(semana_epidem, casos_not_pc)) +
     geom_line(aes(col = "Notificação")) +
     geom_line(aes(y=casos_sin_pc, col = "Sintoma")) +
@@ -131,7 +145,8 @@ for(nome in unique(casos.semana.drs$nome_drs)){
     png(paste0("outputs/casos_por_semana_",nome,".png"))
     p1 <-
         casos.semana.drs %>%
-        filter(semana_epidem < max(semana_epidem) & nome_drs == nome) %>%
+        ##filter(semana_epidem < max(semana_epidem) & nome_drs == nome) %>%
+        filter(nome_drs == nome) %>%
         ggplot(aes(semana_epidem, casos_not_pc)) +
         geom_line(aes(col = "Notificação"), size = 1.25) +
         geom_line(aes(y=casos_sin_pc, col = "Sintoma"), size = 1.25) +
@@ -152,7 +167,7 @@ for(nome in unique(casos.semana.drs$nome_drs)){
 ## Obitos
 png("outputs/obitos_por_semana.png", width =900, height = 600)
 casos.semana.drs %>%
-    filter(semana_epidem < max(semana_epidem)-1) %>%
+    ##filter(semana_epidem < max(semana_epidem)-1) %>%
     ggplot(aes(semana_epidem, obitos_not_pc)) +
     geom_line(aes(col = "Notificação")) +
     geom_line(aes(y=obitos_sin_pc, col = "Sintoma")) +
@@ -168,7 +183,8 @@ for(nome in unique(casos.semana.drs$nome_drs)){
     png(paste0("outputs/obitos_por_semana_",nome,".png"))
     p1 <-
         casos.semana.drs %>%
-        filter(semana_epidem < max(semana_epidem) & nome_drs == nome) %>%
+        ##filter(semana_epidem < max(semana_epidem) & nome_drs == nome) %>%
+        filter(nome_drs == nome) %>%
         ggplot(aes(semana_epidem, obitos_not_pc)) +
         geom_line(aes(col = "Notificação"), size = 1.25) +
         geom_line(aes(y=obitos_sin_pc, col = "Sintoma"), size = 1.25) +
@@ -204,3 +220,6 @@ n.not.dia %>%
     theme_bw()
     ## geom_line(aes(y=diff.cum), col = "red")
 dev.off()
+
+## Voltando ao master
+system("cd ../../../clone_repo_seade_SP/; git checkout master")
