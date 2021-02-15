91c2c0c9aa2948f4f630c41793a887a628a2d9ff
Renato Coutinho <renato.coutinho@gmail.com>
2021-02-15 18:52:50 -0300

    mais opções de layout do facet por drs

--- scripts_R_genericos/seade/seade.R
+++ scripts_R_genericos/seade/seade.R
@@ -22,6 +22,20 @@ grid_drs <- data.frame(
            "DRS 1", "DRS 12", "DRS 4"),
   stringsAsFactors = FALSE
 )
+grid_drs2 <- data.frame(
+  row = c(1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5),
+  col = c(5, 4, 3, 2, 5, 4, 1, 2, 6, 4, 3, 5, 7, 3, 5, 5, 4),
+  name = c("DRS 08 Franca", "DRS 05 Barretos", "DRS 15 São José do Rio Preto", "DRS 02 Araçatuba", "DRS 13 Ribeirão Preto", "DRS 03 Araraquara", "DRS 11 Presidente Prudente", "DRS 09 Marília", "DRS 14 São João da Boa Vista", "DRS 10 Piracicaba", "DRS 06 Bauru", "DRS 07 Campinas", "DRS 17 Taubaté", "DRS 16 Sorocaba", "DRS 01 Grande SP", "DRS 04 Baixada Santista", "DRS 12 Registro"),
+  code = c("DRS 8", "DRS 5", "DRS 15", "DRS 2", "DRS 13", "DRS 3", "DRS 11", "DRS 9", "DRS 14", "DRS 10", "DRS 6", "DRS 7", "DRS 17", "DRS 16", "DRS 1", "DRS 4", "DRS 12"),
+  stringsAsFactors = FALSE
+)
+grid_drs3 <- data.frame(
+  row = c(1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6),
+  col = c(5, 4, 3, 2, 5, 4, 6, 1, 2, 4, 5, 6, 3, 7, 5, 6, 8, 5, 4, 6, 6, 5),
+  name = c("DRS 08 Franca", "DRS 05 Barretos", "DRS 15 São José do Rio Preto", "DRS 02 Araçatuba", "DRS 13 Ribeirão Preto", "DRS 03 Araraquara", "DRS 14 São João da Boa Vista", "DRS 11 Presidente Prudente", "DRS 09 Marília", "DRS 10 Piracicaba", "DRS 07 Campinas", "Grande SP Norte", "DRS 06 Bauru", "Grande SP Leste", "Grande SP Oeste", "Município de São Paulo", "DRS 17 Taubaté", "Grande SP Sudoeste", "DRS 16 Sorocaba", "Grande SP Sudeste", "DRS 04 Baixada Santista", "DRS 12 Registro"),
+  code = c("DRS 8", "DRS 5", "DRS 15", "DRS 2", "DRS 13", "DRS 3", "DRS 14", "DRS 11", "DRS 9", "DRS 10", "DRS 7", "GSP N", "DRS 6", "GSP L", "GSP O", "MSP", "DRS 17", "GSP SO", "DRS 16", "GSP SE", "DRS 4", "DRS 12"),
+  stringsAsFactors = FALSE
+)
 #geofacet::grid_preview(grid_drs)
 
 # checkout local de https://github.com/seade-R/dados-covid-sp
@@ -93,6 +107,16 @@ p_estado2 <- ggplot(dados_estado) +
          y="internações nos últimos 7 dias",
          title="Internações nos últimos 7 dias",
          caption="fonte: Seade") +
-    facet_geo(~nome_drs, grid=grid_drs, scales="free")
+    facet_geo(~nome_drs, grid=grid_drs2, scales="free")
 
+p_estado3 <- dados_seade %>%
+    filter(nome_drs != "Estado de São Paulo") %>%
+    ggplot() +
+    geom_line(aes(x=datahora, y=internacoes_7d)) +
+    scale_x_date(date_breaks = "2 months", date_labels = "%b") +
+    labs(x="data de notificação",
+         y="internações nos últimos 7 dias",
+         title="Internações nos últimos 7 dias",
+         caption="fonte: Seade") +
+    facet_geo(~nome_drs, grid=grid_drs3, scales="free")
 
